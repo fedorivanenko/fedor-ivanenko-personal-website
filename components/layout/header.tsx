7bb6feb5dee4ebe55e665f4915cecde91c25b298
Fedor Ivanenko <fiodor.ivanenko@gmail.com>
2025-01-24 19:31:59 +0200

    fix header link

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -5,14 +5,14 @@ export default function Header() {
   
   return (
     <header className="mr-auto mb-32 top-0">
-      <h1 className="font-medium">
-        <Link href="/" className="flex space-x-1.5 items-baseline max-w-min">
+      <Link href="/" className="flex space-x-1.5 items-baseline max-w-min">
+        <h1 className="font-medium">
           <span className="inline-block leading-none whitespace-nowrap">
             Fedor Ivanenko
           </span>
           <Pulse />
-        </Link>
-      </h1>
+        </h1>
+      </Link>
       <p className="text-muted-foreground">Design Engineer</p>
     </header>
   );
