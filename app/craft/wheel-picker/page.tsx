0643202b18b0575dc7613880e8a418a910d2a29f
Fedor Ivanenko <fiodor.ivanenko@gmail.com>
2025-10-13 00:53:19 +0200

    fix: add section

--- app/craft/wheel-picker/page.tsx
+++ app/craft/wheel-picker/page.tsx
@@ -1,9 +1,12 @@
+import { Section } from "@/components/layout/section";
 import { BaseFieldTest } from "./component";
 
 export default function Page() {
     return (
         <main>
+            <Section>
             <BaseFieldTest/>
+            </Section>
         </main>
     )
 }
