3153aacbd989bc324b382867520fb494ec11db4e
Fedor Ivanenko <fiodor.ivanenko@gmail.com>
2025-10-13 21:41:23 +0200

    feat: update wheelPicker

--- app/craft/wheel-picker/component.tsx
+++ app/craft/wheel-picker/component.tsx
@@ -5,7 +5,7 @@ import { Form } from "@base-ui-components/react/form";
 import { Field } from "@base-ui-components/react/field";
 import { useWheel } from "@use-gesture/react";
 
-export interface WheelPickerOptions {
+export interface WheelPickerOption {
   value: string;
   label: string;
 }
@@ -18,8 +18,38 @@ export interface WheelPickerProps {
   ariaRequired?: React.AriaAttributes["aria-required"];
   ariaDisabled?: React.AriaAttributes["aria-disabled"];
   disabled?: boolean;
+  settings? : WheelPickerSettings
+  options? : WheelPickerOption[]
 }
 
+export interface WheelPickerSettings {
+  treshHold: number
+  throttle: number
+}
+
+const shiftDown = (prev: number[]) => {
+  const next = [...prev];
+  const last = next.pop()!;
+  next.unshift(last);
+  return next;
+};
+
+const shiftUp = (prev: number[]) => {
+  const next = [...prev];
+  const first = next.shift()!;
+  next.push(first);
+  return next;
+};
+
+const createCenteredPositions = (length: number): number[] => {
+  const mid = Math.floor(length / 2);
+  return Array.from({ length }, (_, i) => {
+    if (i <= mid) return i;
+    return i - length;
+  });
+};
+
+//Core
 function WheelPicker({
   value,
   onPick,
@@ -27,22 +57,67 @@ function WheelPicker({
   onBlur,
   ariaRequired,
   ariaDisabled,
-  disabled,
+  //disabled, //TODO: create a disabled state
+  options = [
+    { value: "JAN", label: "January" },
+    { value: "FEB", label: "February" },
+    { value: "MAR", label: "March" },
+    { value: "APR", label: "April" },
+    { value: "MAY", label: "May" },
+    { value: "JUN", label: "June" },
+    { value: "JUL", label: "July" },
+    { value: "AUG", label: "August" },
+    { value: "SEP", label: "September" },
+    { value: "OCT", label: "October" },
+    { value: "NOV", label: "November" },
+    { value: "DEC", label: "December" }
+  ]
+  ,
+  settings = {
+    treshHold: 32,
+    throttle: 50
+  }
 }: WheelPickerProps) {
+  
+  const [optionPositions, setOptionPositions] = React.useState<number[]>(
+    () => createCenteredPositions(options.length)
+  );
+
   const ref = React.useRef<HTMLDivElement>(null);
+  const eY = React.useRef<number>(0)
+  const prevDir = React.useRef<number>(0)
+
+  const lastUpdate = React.useRef<number>(0);
+
+  React.useEffect(()=>{
+    onPick(options[optionPositions.indexOf(0)].value)
+  },[optionPositions, onPick, options])
 
   useWheel(
-    ({ delta: [, dy], event }) => {
+    ({ direction: [, dirY], delta: [, dY], event }) => {
       event.preventDefault();
-      console.log(dy);
+      eY.current += Math.abs(dY);
+      
+      if (dirY !== prevDir.current && dirY !== 0) {
+        prevDir.current = dirY;
+        eY.current = 0;
+        return;
+      }
+      
+      if (eY.current > settings.treshHold) {
+        const now = Date.now();
+        if (now - lastUpdate.current >= settings.throttle) {
+          setOptionPositions(dirY > 0 ? shiftDown : shiftUp);
+          lastUpdate.current = now;
+          eY.current = 0;
+        }
+      }
     },
     {
       target: ref,
       eventOptions: { passive: false },
     }
   );
-
-  console.log("disbaled:", disabled);
 
   return (
     <div
@@ -53,20 +128,25 @@ function WheelPicker({
       role="listbox"
       aria-required={ariaRequired}
       aria-disabled={ariaDisabled}
-      className="border rounded p-2 w-40"
+      className="border rounded p-2 w-40 h-10 relative mb-12"
     >
-      {["US", "DE", "FR"].map((option) => (
+      {options.map((option, index) => (
         <div
           tabIndex={-1}
-          key={option}
+          key={option.label}
           role="option"
-          aria-selected={value === option}
-          className={`cursor-pointer p-1 ${
-            value === option ? "bg-accent text-white" : ""
+          aria-selected={value === option.value}
+          data-position={optionPositions[index]}
+          style={{
+            transform: `translateY(${32 * optionPositions[index]}px)`,
+            opacity: Math.abs(optionPositions[index]) >= 2 ? 0 : 1
+          }}
+          className={`absolute top-1 left-2 h-8 cursor-pointer p-1 transition-all duration-200 ease-out ${
+            value === option.value ? "bg-accent text-white" : ""
           }`}
-          onClick={() => onPick(option)}
+          //onClick={() => onPick(option.value)}
         >
-          {option}
+          {option.label}
         </div>
       ))}
     </div>
@@ -77,6 +157,7 @@ type ControlWithRef = Field.Control.Props & {
   ref?: React.Ref<HTMLDivElement>;
 };
 
+//Base UI Adapter
 function BaseFieldTest() {
   const [errors, setErrors] = React.useState({});
 
