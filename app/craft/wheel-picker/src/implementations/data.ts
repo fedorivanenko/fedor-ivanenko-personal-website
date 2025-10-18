3dda051aa65bf5ee89b277f87d84fde4d363106a
Fedor Ivanenko <fiodor.ivanenko@gmail.com>
2025-10-18 23:42:54 +0200

    change eg data

--- app/craft/wheel-picker/src/implementations/data.ts
+++ app/craft/wheel-picker/src/implementations/data.ts
@@ -1,22 +1,22 @@
 import { WheelPickerOption } from "../wheel-picker";
 import * as z from "zod";
 
-export const monthOptions = [
-    { value: "", label: "" },
-    { value: "JAN", label: "January" },
-    { value: "FEB", label: "February" },
-    { value: "MAR", label: "March" },
-    { value: "APR", label: "April" },
-    { value: "MAY", label: "May" },
-    { value: "JUN", label: "June" },
-    { value: "JUL", label: "July" },
-    { value: "AUG", label: "August" },
-    { value: "SEP", label: "September" },
-    { value: "OCT", label: "October" },
-    { value: "NOV", label: "November" },
-    { value: "DEC", label: "December" },
-  ] satisfies WheelPickerOption[];
+export const monthOptions: WheelPickerOption[] = [
+  { value: "", label: "" },
+  { value: "JAN", label: "January" },
+  { value: "FEB", label: "February" },
+  { value: "MAR", label: "March" },
+  { value: "APR", label: "April" },
+  { value: "MAY", label: "May" },
+  { value: "JUN", label: "June" },
+  { value: "JUL", label: "July" },
+  { value: "AUG", label: "August" },
+  { value: "SEP", label: "September" },
+  { value: "OCT", label: "October" },
+  { value: "NOV", label: "November" },
+  { value: "DEC", label: "December" },
+];
 
-  export const formSchema = z.object({
-    month: z.string().min(1, "Pick a month"),
-  });
+export const formSchema = z.object({
+  month: z.string().min(1, "Pick a month"),
+});
