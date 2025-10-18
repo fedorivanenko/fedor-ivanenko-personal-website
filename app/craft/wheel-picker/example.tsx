5fd3c1144ad628048ca911829d905b722adf7831
Fedor Ivanenko <fiodor.ivanenko@gmail.com>
2025-10-18 23:43:27 +0200

    update example

--- app/craft/wheel-picker/example.tsx
+++ app/craft/wheel-picker/example.tsx
@@ -1,10 +1,16 @@
 "use client";
 
+import * as React from 'react';
 import { Controller, useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { WheelPicker } from "./src/wheel-picker";
-import { monthOptions, formSchema } from "./src/implementations/data";
 import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { WheelPicker, WheelPickerHandle, WheelPickerWrapper } from "./src/wheel-picker";
+import { monthOptions, formSchema } from "./src/implementations/data";
+import { toast } from "sonner"
+
+export function Button(props: React.ButtonHTMLAttributes<HTMLButtonElement>) {
+  return <button className="border text-lg sm:text-sm w-full py-1 cursor-pointer border-border rounded hover:bg-accent/20 transition-colors" {...props} />;
+}
 
 function Example() {
   const form = useForm<z.infer<typeof formSchema>>({
@@ -17,38 +23,44 @@ function Example() {
 
   function onSubmit(data: z.infer<typeof formSchema>) {
     console.log("submitted", data);
+    const result = monthOptions.find(item => item.value === data.month)?.label
+    toast.message(`${result} will come`)
   }
+
+  const pickerRef = React.useRef<WheelPickerHandle | null>(null);
 
   return (
     <form
       onSubmit={form.handleSubmit(onSubmit)}
+      onReset={() => {
+        form.reset();
+        pickerRef.current?.reset();
+      }}
       className="flex flex-col items-center space-y-6 flex-1"
     >
       <Controller
         name="month"
         control={form.control}
         render={({ field, fieldState }) => (
-          <div
-            data-invalid={fieldState.invalid}
-            className="border border-border flex data-[invalid=true]:ring-destructive ring-2 ring-offset-4 ring-offset-card ring-transparent transition-all duration-250 rounded h-48 w-40 gap-1"
+          <WheelPickerWrapper
+            invalid={fieldState.invalid}
+            className="w-60 sm:w-40 h-64 sm:h-48 ring-offset-card text-xl sm:text-base"
           >
             <WheelPicker
+              forwardedRef={pickerRef}
               callbackRef={field.ref}
-              value={field.value}
               options={monthOptions}
               onPick={field.onChange}
               onBlur={field.onBlur}
               disabled={field.disabled}
             />
-          </div>
+          </WheelPickerWrapper>
         )}
       />
-      <button
-        type="submit"
-        className="border w-40 border-border rounded hover:bg-accent/20 transition-colors"
-      >
-        Submit
-      </button>
+      <div className="flex flex-col space-y-1 w-60 sm:w-40">
+        <Button type="submit">Submit</Button>
+        <Button type="reset">Reset</Button>
+      </div>
     </form>
   );
 }
