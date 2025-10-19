58157f1022ae07eec60daea0ff9a95f1c327e204
Fedor Ivanenko <fiodor.ivanenko@gmail.com>
2025-10-19 19:38:37 +0200

    fix arrow in header

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export function Header() {
   return (
     <header>
-      <Link href={'/'} target="_self">{"<-"} back</Link>
+      <Link href={'/'} target="_self">{"<---"}</Link>
     </header>
   );
 }
