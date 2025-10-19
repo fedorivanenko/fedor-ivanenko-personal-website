7aadcbdd61d9659d57b4e6ac6db344d367d05af1
Fedor Ivanenko <fiodor.ivanenko@gmail.com>
2025-10-19 02:33:38 +0200

    fix metadata link

--- config/site.ts
+++ config/site.ts
@@ -1,10 +1,6 @@
-const baseUrl = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : 'http://localhost:3000';
-
 export const siteConfig = {
   name: "Fedor Ivanenko",
-  url: baseUrl,
+  url: "https://www.fedor.studio/",
   description: "designer + engineer",
   author: {
     name: "Fedor Ivanenko",
