23eddb2a0ec42b9171cb92f17abb07800c6a6741
Fedor Ivanenko <fiodor.ivanenko@gmail.com>
2025-11-18 00:07:54 +0200

    add oklch play page

---
+++ app/play/oklch-border/page.tsx
@@ -0,0 +1,13 @@
+import { Section } from "@/components/layout/section";
+
+import s from "./border.module.css";
+
+export default function PLatOKLCHBordersPage() {
+  return (
+    <article>
+      <Section>
+        <div className={s.oklch}>OKLCH Color</div>
+      </Section>
+    </article>
+  );
+}
