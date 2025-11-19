0ebf5b5f7e0a99b09491cb268cd9fbcdce954b1a
Fedor Ivanenko <fiodor.ivanenko@gmail.com>
2025-11-19 23:02:16 +0200

    add theme switcher

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,9 +1,10 @@
+import ThemeToggle from "../theme-toggle";
 import { InlineLinkButton } from "../ui/button";
 
 export default function Footer() {
   return (
     <footer className="flex flex-col animate-blur-fade mx-auto mt-auto w-full px-5 pb-5 text-xs text-muted-foreground pt-20 md:pt-40 stagger-2 ">
-      <div className="max-w-[var(--content-width)] w-full mx-auto flex flex-row flex-wrap gap-x-4 sm:gap-x-6 gap-y-2 items-baseline">
+      <div className="max-w-[var(--content-width)] w-full mx-auto flex flex-row flex-wrap gap-x-4 sm:gap-x-6 gap-y-2 items-center">
         <span>{new Date().getFullYear()}</span>
         <span>Talk is cheap, show me your code</span>
         <InlineLinkButton
@@ -14,6 +15,7 @@ export default function Footer() {
         >
           website code
         </InlineLinkButton>
+        <ThemeToggle/>
       </div>
     </footer>
   );
---
+++ components/theme-toggle.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import { useTheme } from "next-themes";
+
+import Sun from "@/components/icons/sun.svg";
+import Moon from "@/components/icons/moon.svg";
+import Laptop from "@/components/icons/laptop.svg";
+
+interface themeOptionInterface {
+  label: string;
+  value: string;
+  icon: React.ComponentType<React.SVGProps<SVGSVGElement>>;
+}
+
+const MiniButton = ({
+  label,
+  value,
+  icon: Icon,
+  theme,
+  setTheme,
+}: themeOptionInterface & {
+  theme: string | undefined;
+  setTheme: (v: string) => void;
+}) => {
+  return (
+    <span>
+      <input
+        aria-label={label}
+        id={`theme-switch-${value}`}
+        type="radio"
+        value={value}
+        checked={theme === value}
+        onChange={() => setTheme(value)}
+        className="sr-only peer"
+      />
+      <label
+        htmlFor={`theme-switch-${value}`}
+        className="flex cursor-pointer items-center peer-checked:text-foreground"
+      >
+        <span className="sr-only">{label}</span>
+        <Icon className="size-3.5" />
+      </label>
+    </span>
+  );
+};
+
+const options = [
+  { value: "system", label: "System", icon: Laptop },
+  { value: "light", label: "Light", icon: Sun },
+  { value: "dark", label: "Dark", icon: Moon },
+] satisfies themeOptionInterface[];
+
+export default function ThemeToggle() {
+  const { theme, setTheme } = useTheme();
+
+  return (
+    <fieldset className="flex gap-2">
+      <legend className="sr-only">Select a display theme:</legend>
+
+      {options.map((opt) => (
+        <MiniButton
+          key={opt.value}
+          {...opt}
+          theme={theme}
+          setTheme={setTheme}
+        />
+      ))}
+    </fieldset>
+  );
+}
