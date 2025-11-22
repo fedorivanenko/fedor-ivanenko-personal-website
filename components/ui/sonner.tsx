5a248e265b9fc76466d4e168c48fdebca2f8d4c8
Fedor Ivanenko <fiodor.ivanenko@gmail.com>
2025-11-22 20:16:32 +0200

    style fixes

--- components/ui/sonner.tsx
+++ components/ui/sonner.tsx
@@ -15,7 +15,7 @@ const Toaster = ({ ...props }: ToasterProps) => {
       toastOptions={{
         classNames: {
           toast:
-            "group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg",
+            "group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg !py-2 !px-4",
           description: "group-[.toast]:text-muted-foreground",
           actionButton:
             "group-[.toast]:bg-primary group-[.toast]:text-primary-foreground",
@@ -28,7 +28,7 @@ const Toaster = ({ ...props }: ToasterProps) => {
           "--normal-bg": "var(--card)",
           "--normal-text": "var(--foreground)",
           "--normal-border": "var(--card-border)",
-          "--border-radius": "var(--radius)",
+          "--border-radius": 0,
         } as React.CSSProperties
       }
       {...props}
