08b178a82130235b94a1eaebb26636f2c6699ab7
Fedor Ivanenko <fiodor.ivanenko@gmail.com>
2025-11-22 20:41:43 +0200

    iosevka font

--- lib/fonts.ts
+++ lib/fonts.ts
@@ -1,33 +1,18 @@
-import { Geist, Geist_Mono, Barlow } from 'next/font/google'
+import { Barlow } from 'next/font/google'
 import localFont from 'next/font/local'
 
-export const etBook = localFont({
+export const iosevka = localFont({
   src: [
     {
-      path: '../app/fonts/et-book-roman-line-figures.woff',
+      path: '../app/fonts/Iosevka-Aile-01.woff2',
       weight: '400',
       style: 'normal'
-    },
-    {
-      path: '../app/fonts/et-book-semi-bold-old-style-figures.woff',
-      weight: '600',
-      style: 'normal'
-    },
-    {
-      path: '../app/fonts/et-book-bold-line-figures.woff',
-      weight: '700',
-      style: 'normal'
-    },
-    {
-      path: '../app/fonts/et-book-display-italic-old-style-figures.woff',
-      weight: '400',
-      style: 'italic'
-    },
-
+    }
   ],
-  variable: '--font-et-book',
-  display: 'swap',
+    variable: '--font-iosevka-aile',
+    display: 'swap',
 })
+
 
 export const commit = localFont({
   src: [
@@ -45,17 +30,5 @@ export const barlow = Barlow({
   subsets: ['latin'],
   weight: ['400', '700'],
   variable: '--font-barlow-sans',
-  display: 'swap',
-})
-
-export const geist = Geist({
-  subsets: ['latin'],
-  variable: '--font-geist-sans',
-  display: 'swap',
-})
-
-export const geistMono = Geist_Mono({
-  subsets: ['latin'],
-  variable: '--font-geist-mono',
   display: 'swap',
 })
